70dfbcab6ee6465e6076a942d813bd54763d4b1d
adryanbartolome <adryanbartolome8123@gmail.com>
2024-03-16 11:33:05 -0700

    Applied JavaScript topics that were learned in class such as operators and concatenate strings with variables. Also added overview and requirements in the readme doc.

---
+++ index.js
@@ -0,0 +1,22 @@
+// The code must equal to 10 - 40 - 39
+
+// Create string with message
+
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Assign and create the variables
+
+// Assign variable for number 10
+const num1 = 2 * 5;
+
+// Assign variable for number 40
+const num2 = 80 / 2;
+
+// Assign variable for number 39
+const num3 = 35 + 4;
+
+// Console log the message and combination
+console.log(`${message} ${num1} - ${num2} - ${num3} `)
+
+// You can also create an alert message for web page
+alert(`${message} ${num1} - ${num2} - ${num3} `)
